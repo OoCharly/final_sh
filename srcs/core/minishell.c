3add18616afaa56f7f820ff6601f92596f1c1c3f
Jordan MUNOZ <jmunoz@e3r8p10.42.fr>
2016-12-19 18:23:25 +0100

    without ft_auote in minishell.c

--- srcs/core/minishell.c
+++ srcs/core/minishell.c
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 17:43:47 by tboos             #+#    #+#             */
-/*   Updated: 2016/12/19 18:22:07 by jmunoz           ###   ########.fr       */
+/*   Updated: 2016/12/19 18:23:13 by jmunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,8 +80,7 @@ void		ft_run_command(t_config *config)
 	config->shell_state = RUNNING_COMMAND;
 	if ((config->chimera = ft_lexer(config->command)))
 	{
-		if (!ft_quote(config->chimera, config)
-				|| !ft_insert_loop(config->chimera)
+		if (!ft_insert_loop(config->chimera)
 				|| !ft_herringbone(config->chimera, config))
 			ft_freelist(&config->chimera);
 		else
