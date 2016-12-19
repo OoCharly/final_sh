55d79de3432093f81cb4839f71f6c832d5b3cb0e
Jordan MUNOZ <jmunoz@e3r8p10.42.fr>
2016-12-19 18:19:26 +0100

    with minishell.c

--- srcs/core/minishell.c
+++ srcs/core/minishell.c
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 17:43:47 by tboos             #+#    #+#             */
-/*   Updated: 2016/12/16 14:49:07 by maxpetit         ###   ########.fr       */
+/*   Updated: 2016/12/19 15:18:29 by jmunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,8 +83,8 @@ void		ft_run_command(t_config *config)
 	config->shell_state = RUNNING_COMMAND;
 	if ((config->chimera = ft_lexer(config->command)))
 	{
-		if (!ft_quote(config->chimera, config)
-				|| !ft_insert_loop(config->chimera)
+		if (//!ft_quote(config->chimera, config)
+				!ft_insert_loop(config->chimera, config)
 				|| !ft_herringbone(config->chimera, config))
 			ft_freelist(&config->chimera);
 		else
