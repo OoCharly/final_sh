039e68e7757f3075a60d7f84dae79656a162e59a
Dflk <baran.romain@gmail.com>
2017-01-07 00:38:37 +0100

    fix bg

--- srcs/builtins/jobs.c
+++ srcs/builtins/jobs.c
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 08:45:29 by tboos             #+#    #+#             */
-/*   Updated: 2017/01/05 18:45:59 by maxpetit         ###   ########.fr       */
+/*   Updated: 2017/01/07 00:38:09 by rbaran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,13 +52,15 @@ static void		ft_continue(t_config *config, char *description, int mode)
 				CR_ERROR);
 	else if (!(target = ft_extract_job(config, description)))
 		ft_error(mode == JOBS_FG ? "fg" : "bg", description, "no such job",
-				CR_ERROR);
-	p = target;
-	while ((p = p->next))
-		kill(*((pid_t*)p->data), SIGCONT);
-	ft_freegiveone((void**)&config->fg_sentence);
-	if (mode == JOBS_FG)
+				CR_ERROR);	
+	if (target && (mode == JOBS_FG || mode == JOBS_BG))
+	{
+		p = target;
+		while ((p = p->next))
+			kill(*((pid_t*)p->data), SIGCONT);
+		ft_freegiveone((void**)&config->fg_sentence);
 		ft_wait_sentence(target, config);
+	}
 }
 
 void			ft_jobs(char **argv, t_config *config)
