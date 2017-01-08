8cb7b9427ad631a79ac2b465e226ef88104a02a2
nyhu <toussaint.boos@gmail.com>
2017-01-08 02:49:38 +0100

    ajout comment

--- srcs/core/terminit.c
+++ srcs/core/terminit.c
@@ -2,7 +2,8 @@
 
 /*
 **Searches the terminal name in config->env. If variable TERM exists,
-**initializes the termcaps.
+**initializes the termcaps. usleep is depreciated in POSIX,
+** we should use nanosleep
 */
 
 void	ft_termcaps_init(t_config *config)
