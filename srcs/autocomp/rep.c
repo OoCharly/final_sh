e21a814d6420e36f9d54d6d0b1f58519708dc5bb
Charles-henry DESVERNAY <cdesvern@e1r12p14.42.fr>
2017-01-26 17:53:20 +0100

    free to FREE

--- srcs/autocomp/rep.c
+++ srcs/autocomp/rep.c
@@ -6,7 +6,7 @@
 /*   By: jmunoz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 15:57:39 by jmunoz            #+#    #+#             */
-/*   Updated: 2017/01/06 13:25:50 by jmunoz           ###   ########.fr       */
+/*   Updated: 2017/01/26 17:38:40 by cdesvern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ int				ft_rep(t_stream *stream, char *b)
 		ret += ft_check_insert(&arg, j++, stream->config);
 	if (!ret)
 	{
-		free(arg);
+		FREE((void**)&arg);
 		return (0);
 	}
 	add = ft_strtabchrjoin(arg, ' ');
