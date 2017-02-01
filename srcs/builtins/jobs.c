af65f6cdef0eeeb76ae3e20fc9320da75e562c94
Romain BARAN <rbaran@e1r11p14.42.fr>
2017-02-01 13:50:14 +0100

    controlling childs state (not finished)

--- srcs/builtins/jobs.c
+++ srcs/builtins/jobs.c
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 08:45:29 by tboos             #+#    #+#             */
-/*   Updated: 2017/01/26 16:16:59 by cdesvern         ###   ########.fr       */
+/*   Updated: 2017/02/01 12:11:19 by rbaran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,9 +55,9 @@ static void		ft_continue(t_config *config, char *description, int mode)
 		return ;
 	p = target;
 	if (mode == JOBS_FG)
-		tcsetpgrp(0, *((pid_t*)target->next->data));
+		tcsetpgrp(0, getpgid(*((pid_t*)target->next->data)));
 	while ((p = p->next))
-		kill(*((pid_t*)p->data), SIGCONT);
+		kill(-getpgid(*((pid_t*)target->next->data)), SIGCONT);
 	if (mode == JOBS_BG)
 		config->dot_sequence = 'b';
 	config->fg_sentence = ft_strdup(((t_sentence*)target->data)->sentence);
