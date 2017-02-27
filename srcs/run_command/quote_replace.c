80131dbcf6a5819d7bffbfd2e9d4d5627c3bfdc6
Charles-henry DESVERNAY <cdesvern@e1r13p11.42.fr>
2017-02-27 16:19:47 +0100

    hotfix quote_replace

--- srcs/run_command/quote_replace.c
+++ srcs/run_command/quote_replace.c
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 09:21:44 by tboos             #+#    #+#             */
-/*   Updated: 2017/02/17 15:59:51 by cdesvern         ###   ########.fr       */
+/*   Updated: 2017/02/27 16:17:55 by cdesvern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ char	*ft_quotehandle(char **str, t_config *config)
 	j = 0;
 	while ((*str)[j])
 	{
-		if ((((*str)[j] == '~' || (*str)[j] == '$') && ((*str)[j + 1]))
-				&& !(*str = ft_envvarinsert(*str, &j, config)))
+		if ((((*str)[j] == '~' || ((*str)[j] == '$' && (*str)[j + 1]))
+				&& !(*str = ft_envvarinsert(*str, &j, config))))
 			return (NULL);
 		else if ((*str)[j] == '\'')
 			j = ft_dodge_quote(*str, j);
